6cd9c5b8b2764cf6d674b982d63c55bba15e3ac1
Faun <Faun.schuetz@gmail.com>
2020-11-02 11:25:36 +0000

    CA1 - Commit.

--- src/Song.java
+++ src/Song.java
@@ -1,2 +1,54 @@
+/* By: Faun Schutz
+ * Song.java
+ * Start:
+ *
+ */
 public class Song {
+    private static int trackID;
+    private int trackNumber;
+    private String title;
+    private String artist;
+    private String genre;
+    private int duration;
+    private int releaseYear;
+
+    Song(String title, String artist, String genre, int duration, int releaseYear){
+        setTrackNumber(trackNumber);
+        setTitle(title);
+        setArtist(artist);
+        setGenre(genre);
+        setDuration(duration);
+        setReleaseYear(releaseYear);
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setArtist(String artist){
+        this.artist = artist;
+    }
+    public String getArtist(){
+        return artist;
+    }
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+    public String getGenre(){
+        return genre;
+    }
+    public void setDuration(int duration){
+        this.duration = duration;
+    }
+    public int getDuration(){
+        return duration;
+    }
+    public void setReleaseYear(int releaseYear){
+        this.releaseYear = releaseYear;
+    }
+    public int getReleaseYear() {
+        return releaseYear;
+    }
 }
